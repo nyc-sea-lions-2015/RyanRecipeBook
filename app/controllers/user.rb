043a9ad33dc8c9c7a3ea88c5b886c1199f6e5c5e
Ryan <ryanimmesberger@gmail.com>
2015-04-01 10:48:39 -0400

    create login post route to log user in

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,15 @@
 get '/login' do
   erb: 'auth/login'
 end
+
+post 'login' do
+  user = User.find_by(name: params[:name])
+
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    session[:error] = "Login failed"
+    redirect '/login'
+  end
+end
