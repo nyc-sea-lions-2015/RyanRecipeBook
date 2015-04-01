e693765141b4d234e9f4b0c9c29ee3571dae8603
Ryan <ryanimmesberger@gmail.com>
2015-04-01 12:33:35 -0400

    added get new recipe route

--- app/controllers/recipe.rb
+++ app/controllers/recipe.rb
@@ -7,3 +7,11 @@ get '/recipe/:id' do
   @cur_recipe = Recipe.find_by(id: params[:id])
   erb :'recipe/recipe'
 end
+
+get '/recipe/new' do
+  if current_user
+    erb :'recipe/new'
+  else
+    redirect '/signup'
+  end
+end
