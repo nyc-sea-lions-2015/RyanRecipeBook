9cb435d7a83f20884be317a12649f95158309856
Ryan <ryanimmesberger@gmail.com>
2015-04-01 13:33:18 -0400

    created new recipe view

--- app/controllers/recipe.rb
+++ app/controllers/recipe.rb
@@ -3,11 +3,6 @@ get '/recipes' do
   erb :'recipe/all_recipes'
 end
 
-get '/recipe/:id' do
-  @cur_recipe = Recipe.find_by(id: params[:id])
-  erb :'recipe/recipe'
-end
-
 get '/recipe/new' do
   if current_user
     erb :'recipe/new'
@@ -15,3 +10,9 @@ get '/recipe/new' do
     redirect '/signup'
   end
 end
+
+get '/recipe/:id' do
+  @cur_recipe = Recipe.find_by(id: params[:id])
+  erb :'recipe/recipe'
+end
+
