31c503e2c8333776e53040655fe4bd93cede0509
Ryan <ryanimmesberger@gmail.com>
2015-04-01 17:32:34 -0400

    added create new ingredients to data table on new recipe page

--- app/controllers/ingredient.rb
+++ app/controllers/ingredient.rb
@@ -1,1 +1,11 @@
+get '/ingredient/new' do
+  erb :new_ingredient, layout: false
+end
 
+post '/ingredient/new' do
+  @new_ingredient = Ingredient.new(name: params[:name])
+
+  unless @new_ingredient.save
+    [404, "did not save ingredient"]
+  end
+end
